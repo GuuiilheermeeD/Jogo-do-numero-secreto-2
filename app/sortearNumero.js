d70b8f969c3de03a7b3cc0a9ff11a6bc477fee3b
Your Name <you@example.com>
2024-05-16 21:26:23 -0300

    reconhecimento de voz funcionando

---
+++ app/sortearNumero.js
@@ -0,0 +1,15 @@
+const menorValor = 1
+const maiorValor = 100
+const numeroSecreto = gerarNumeroAleatorio()
+const elementoMenorValor = document.getElementById('menor-valor')
+const elementoMaiorValor = document.getElementById('maior-valor')
+
+function gerarNumeroAleatorio(){
+   return parseInt(Math.random()* maiorValor + 1)
+}
+
+elementoMenorValor.innerHTML = menorValor
+elementoMaiorValor.innerHTML = maiorValor
+
+
+console.log('O Numero Secreto é: ', numeroSecreto);
